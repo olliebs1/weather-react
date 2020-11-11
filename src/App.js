1d5476373cc29cd88694ed1282e7071cb17964a8
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-11 14:16:42 +0000

    backgrounds and search bar added - css added for app  and body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import api_key from './api_key'
+import './index.css';
+import api_key from './api_key';
 
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type='text'
+            className='search-bar'
+            placeholder='Search...'
+          />
+        </div>
+      </main>
     </div>
   );
 }
