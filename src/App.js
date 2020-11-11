920ceb375b70292f052ecce6258105f08ac9b64a
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-11 13:49:32 +0000

    api_key reated and imported into App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import api_key from './api_key'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api_key.js
@@ -0,0 +1,7 @@
+const api_key = {
+    key: "be82730badf4d963298485501f4ffadc",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
+
+  export default api_key;
